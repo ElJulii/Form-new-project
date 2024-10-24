02c8b852e7fba79a272c9f43a0d7a5bb7e07fe7e
Julian <juli2003juli@gmail.com>
2024-10-24 11:59:18 +0300

    I change the name of dialog to modul and I imporve the modul

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 const button_new_project = document.getElementById('button-new-project');
 
 //dialog
-const dialog_new_project = document.getElementById('dialog-new-project');
+const model_new_project = document.getElementById('model-new-project');
 
 //inputs
 const switch_check = document.getElementById('switch-check');
@@ -12,12 +12,12 @@ const client_id = document.getElementById('client-id');
 const label_client_id = document.querySelector('.last-label')
 
 //labels and inputs users
-const information_label_inputs = document.querySelectorAll('.label__first-dialog');
-const information_input_users = document.querySelectorAll('.first-dialog__input')
+const information_label_inputs = document.querySelectorAll('.label__first-model');
+const information_input_users = document.querySelectorAll('.first-model__input')
 
 
 button_new_project.addEventListener('click', () => {
-    dialog_new_project.setAttribute('open', 'true');
+    model_new_project.setAttribute('open', 'true');
 })
 
 switch_check.addEventListener('change', () => {
