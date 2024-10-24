b3995c97b8f9d4af0443fb0f412afd20d65d5891
Julian <juli2003juli@gmail.com>
2024-10-24 09:55:01 +0300

    switch toggle has functionaluty and and added iput and label for cliend id

--- functions.js
+++ functions.js
@@ -4,6 +4,40 @@ const button_new_project = document.getElementById('button-new-project');
 //dialog
 const dialog_new_project = document.getElementById('dialog-new-project');
 
+//inputs
+const switch_check = document.getElementById('switch-check');
+
+//client ID
+const client_id = document.getElementById('client-id');
+const label_client_id = document.querySelector('.last-label')
+
+//labels and inputs users
+const information_label_inputs = document.querySelectorAll('.label__first-dialog');
+const information_input_users = document.querySelectorAll('.first-dialog__input')
+
+
 button_new_project.addEventListener('click', () => {
     dialog_new_project.setAttribute('open', 'true');
+})
+
+switch_check.addEventListener('change', () => {
+    if (switch_check.checked) {
+        information_label_inputs.forEach(label => {
+            label.style.display = 'none';
+        })
+        information_input_users.forEach(input => {
+            input.style.display = 'none';
+        })
+        client_id.style.display = 'inline';
+        label_client_id.style.display = 'inline'
+    } else {
+        information_label_inputs.forEach(label => {
+            label.style.display = 'inline';
+        })
+        information_input_users.forEach(input => {
+            input.style.display = 'inline';
+        })
+        client_id.style.display = 'none';
+        label_client_id.style.display = 'none'
+    }
 })
